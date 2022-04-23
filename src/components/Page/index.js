4848d72b480dd440e2be3513a13b06706009508c
Jonathan Bedoya <jb_193@hotmail.com>
2022-04-23 09:56:32 -0500

    menu nav

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,8 +1,12 @@
+import { Menu } from "../Menu"
 import { PageWrapper } from "./styles"
 
 
 export const Page = (props) => (
+  <>
     <PageWrapper>
         { props.children }
     </PageWrapper>
+    <Menu/>
+  </>
 )
