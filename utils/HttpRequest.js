892796a96eaba76d4c288622171924fe1864ead9
Jonathan Bedoya <jb_193@hotmail.com>
2022-06-18 11:49:05 -0500

    actualizando project

---
+++ utils/HttpRequest.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export const HTTP_VERBS = {
+    POST: "post",
+    GET: "get",
+    PUT: "put",
+    DELETE: "delete"
+}
+
+export const requestHttp = async () =>{
+    try{
+
+    } catch (error) {
+        
+    }
+}
